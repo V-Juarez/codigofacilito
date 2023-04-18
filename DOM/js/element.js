09256985b65eb3ecd2140a2ca3e9b941abf0c1a2
victoriano <mytacam78@gmail.com>
2023-04-18 00:50:53 -0600

    select element

---
+++ DOM/js/element.js
@@ -0,0 +1,12 @@
+// elemento css
+// id
+const element = document.querySelector('#first-course');
+console.log(element);
+
+// class
+const element1 = document.querySelector('.list-group-item');
+console.log(element1);
+
+// tag
+const element2 = document.querySelector('li');
+console.log(element2);

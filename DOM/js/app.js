3c59e3baee6e2fe2562b0f46422c2d46f65e5436
victoriano <mytacam78@gmail.com>
2023-04-18 00:21:55 -0600

    element id

---
+++ DOM/js/app.js
@@ -0,0 +1,13 @@
+// id
+const title = document.getElementById('title');
+title.innerHTML = 'Cursos';
+
+const description = document.getElementById('description');
+description.innerHTML = "Listado de cursos";
+
+// class
+const items = document.getElementsByClassName('list-group-item');
+for (var i = 0; i < items.length; i++) {
+  let element = items[i];
+  console.log(element)
+}

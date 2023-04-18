95b0e0d96ac422f000de09fafaac118f97f0c0bf
victoriano <mytacam78@gmail.com>
2023-04-18 00:30:42 -0600

    dom tag

--- DOM/js/app.js
+++ DOM/js/app.js
@@ -6,8 +6,18 @@ const description = document.getElementById('description');
 description.innerHTML = "Listado de cursos";
 
 // class
-const items = document.getElementsByClassName('list-group-item');
+// const items = document.getElementsByClassName('list-group-item');
+// for (var i = 0; i < items.length; i++) {
+//   let element = items[i];
+//   console.log(element)
+// }
+
+// etiqueta
+const items = document.getElementsByTagName('li');
 for (var i = 0; i < items.length; i++) {
-  let element = items[i];
-  console.log(element)
+  if (i % 2 == 0) {
+    let element = items[i];
+    element.style.background = '#f2f2f2'
+    console.log(element)
+  }
 }

40daf41aa0e805c97ed0e32d661c7477bbba5daf
victoriano <mytacam78@gmail.com>
2023-04-18 00:42:00 -0600

    elementos por css

--- DOM/js/app.js
+++ DOM/js/app.js
@@ -18,6 +18,32 @@ for (var i = 0; i < items.length; i++) {
   if (i % 2 == 0) {
     let element = items[i];
     element.style.background = '#f2f2f2'
-    console.log(element)
   }
+}
+
+// elemento css
+// id
+const element = document.querySelector('#first-course');
+console.log(element);
+
+// class
+const element1 = document.querySelector('.list-group-item');
+console.log(element1);
+
+// tag
+const element2 = document.querySelector('li');
+console.log(element2);
+
+// reglas de ccs
+const element3 = document.querySelector('div.row > ul.list-group > li');
+console.log(element3);
+
+
+// querySelectoAll
+// par odd
+// impart even
+const items1 = document.querySelectorAll('li:nth-child(even)')
+for (var i = 0; i < items1.length; i++){
+  let element = items1[i]
+  element.style.background = 'gray';
 }

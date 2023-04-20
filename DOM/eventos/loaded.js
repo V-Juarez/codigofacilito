c6eb9c2c547836375e1cd15c9a318183f9447379
victoriano <mytacam78@gmail.com>
2023-04-20 02:50:10 -0600

    DOMContentLoaded

---
+++ DOM/eventos/loaded.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMCONtentLoaded', function(){
+  console.log("Adjuntar informacion ")
+
+/*   pegar todo el codido dentro de la 
+  funcion */
+  console.log('El DOM se construyo')
+})

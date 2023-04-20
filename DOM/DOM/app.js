ddbbbfd2dd2cf9723c41bd30626bdcf056e03e78
victoriano <mytacam78@gmail.com>
2023-04-20 16:02:11 -0600

    agregar elementos dom

--- DOM/DOM/app.js
+++ DOM/DOM/app.js
@@ -21,7 +21,7 @@ form.addEventListener('submit',  function(e) {
   create_card(title, description);
 })
 
-function create_card(title, description) {
+function create_card_by_innerHTML(title, description) {
   let html = `
     <div class="col-sm-6 col-md-4">
       <div class="thumbnail">
@@ -36,4 +36,39 @@ function create_card(title, description) {
     </div>
   `;
   row.innerHTML += html;
+}
+
+function create_card(title, description){
+  let div = document.createElement('div');
+  div.className = 'col-sm-6 col-md-4';
+
+  let thumbnail = document.createElement('div')
+  thumbnail.className = 'thumbnail'
+
+  let caption = document.createElement('div')
+  caption.className = 'caption'
+
+  let h3 = document.createElement('h3')
+  h3.textContent = title;
+
+  let p1 = document.createElement('p')
+  p1.textContent = description
+  
+  let p2 = document.createElement('p')
+  let a = document.createElement('a')
+  a.className = 'btn btn-danger'
+
+  a.textContent = 'eliminar'
+
+  // agregar methos
+  p2.appendChild(a)
+  caption.appendChild(h3)
+  caption.appendChild(p1)
+  caption.appendChild(p2)
+
+  thumbnail.appendChild(caption)
+  div.appendChild(thumbnail)
+
+
+  row.appendChild(div)
 }

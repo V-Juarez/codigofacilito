15999d1aaf7e324e35216349552b2956297b1295
victoriano <mytacam78@gmail.com>
2023-04-23 02:19:15 -0600

    ✨ (js): download
    download photo

--- apis/project1/js/app.js
+++ apis/project1/js/app.js
@@ -1,35 +1,67 @@
 // DOM API
-let video = document.querySelector('.video-source')
+let video = document.querySelector(".video-source");
 // Canvas
-let canvas = document.querySelector('#canvas')
-let context = canvas.getContext('2d')
+let canvas = document.querySelector("#canvas");
+let context = canvas.getContext("2d");
+let botonFoto = document.querySelector("#botton-foto");
+
+// Definir una variable vacia
 let imagenCargada;
 
-async function solicitarPermisosCamara(){
-  // getUserMedia
-  let mediaStream = await navigator.mediaDevices.getUserMedia({ video: true })
-  // API video
-  video.srcObject = mediaStream;
-  video.play()
-}
-
-async function cargarImagen(){
-  let image = document.createElement('img')
-  image.src = '../image/frame.png'
-  await image.decode();
-  imagenCargada = image;
-
-  dibujar()
-}
-
-function dibujar() {
-  if(imagenCargada){
-    context.drawImage(imagenCargada, 0, 0, 100, 200)
-  }
-
-}
-
+redimensionarCanvas();
 solicitarPermisosCamara();
 cargarImagen();
 dibujar();
 
+botonFoto.addEventListener("click", function (ev) {
+  let url = canvas.toDataURL("image/jpeg");
+  let a = document.createElement("a");
+  a.href = url;
+  a.innerHTML = "Descargar";
+  document.querySelector("body").appendChild(a);
+  a.download = "foto.jpg";
+  a.click();
+});
+
+function redimensionarCanvas() {
+  let camara = document.querySelector(".camara");
+  canvas.width = camara.clientWidth;
+  canvas.height = camara.clientHeight;
+}
+
+// Solcitar permisos para la camara => Leer la camara y mostrarla en el video
+async function solicitarPermisosCamara() {
+  // getUserMedia
+  /*
+   * @ getUserMedia => Solicita perisos para la camara y el micro y retorna un
+   *   MediaStream
+   * */
+
+  let mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+  // API video
+  //
+  // Stream sera la fuente de dattos para el elemento video
+  video.srcObject = mediaStream;
+  // reporducir el video con Javascript
+  video.play();
+}
+
+async function cargarImagen() {
+  // node img
+  let image = document.createElement("img");
+  // asignar el frame
+  image.src = "../image/frame.png";
+  // descarga la imagen
+  await image.decode();
+  // guarda la imagen en la varialle
+  imagenCargada = image;
+}
+
+function dibujar() {
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  if (imagenCargada) {
+    context.drawImage(imagenCargada, 0, 0, canvas.width, canvas.height);
+  }
+
+  requestAnimationFrame(dibujar);
+}
